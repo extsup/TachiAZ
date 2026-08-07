617c7659a6c0c3228759b8e8184b5341890de770
extsup <ciklah7878@gmail.com>
2026-08-07 13:40:23 +0700

    Fix when condition syntax in ExtensionHolder

--- app/src/main/java/eu/kanade/tachiyomi/ui/extension/ExtensionHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/extension/ExtensionHolder.kt
@@ -79,9 +79,7 @@ class ExtensionHolder(view: View, override val adapter: ExtensionAdapter) :
             InstallStep.Idle -> when (extension) {
                 is Extension.Installed -> when {
                     extension.hasUpdate -> R.drawable.ic_arrow_down_white_32dp
-                    extension.isObsolete,
-                    extension.isUnofficial,
-                    extension.isRedundant -> R.drawable.ic_warning_white_24dp
+                    extension.isObsolete || extension.isUnofficial || extension.isRedundant -> R.drawable.ic_warning_white_24dp
                     else -> R.drawable.ic_settings_24dp
                 }
                 is Extension.Available -> R.drawable.ic_file_download_black_24dp
